940aff426244ca8cf87c924ab96faf54a02db4fe
shacquille <shacquillee96@gmail.com>
2025-06-16 21:18:57 -0500

    All done bro!

--- python /Lab1.py
+++
@@ -1,4 +0,0 @@
-name = input("Enter your full name:")
-age = input("Enter your age")
-enrollment_Status = input( "Are you currently enrolled in school? (True/False): ")
-print("Hello,",name,"." "you are,", age,"years old." "Currently Enrolled in School:", enrollment_Status)
--- python /shaq1.py
+++
@@ -1,3 +0,0 @@
-name = input("Enter your name:")
-age = input("Enter your age:")
-print("What's up,",name,"." "you are,", age,"years old.")
